dc7b3c745a716f84f6a45ecf622efce70bc22e71
Ben Gee <user4511234@yahoo.com>
2013-06-18 15:10:51 -0700

    simple variations-Ben/Justin

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/SimpleSquare.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/SimpleSquare.java
@@ -8,14 +8,14 @@ public class SimpleSquare
   public static void main(String[] args) throws Exception
   {
     Tortoise.show();
-    Tortoise.setSpeed(8);
-    int sides = 4;
+    Tortoise.setSpeed(10);
+    int sides = 17;
     for (int i = 1; i <= sides; i++)
     {
-      Tortoise.move(50);
-      Tortoise.setPenColor(Colors.Blues.Blue);
-      Tortoise.setPenWidth(2);
-      Tortoise.turn(360 / sides);
+      Tortoise.setPenColor(Colors.getRandomColor());
+      Tortoise.setPenWidth(7);
+      Tortoise.move(23);
+      Tortoise.turn(360.0 / sides);
     }
   }
 }
